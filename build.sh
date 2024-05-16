4546cd78dc97d651a27466825afa522e56533cf0
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2024-05-16 15:32:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -167,8 +167,9 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
-log "\nUsed [j-hc/revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
-log "\nChangelog:"
+#log "\nUsed [j-hc/revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+#log "\nChangelog:"
+## looks bloated in telegram updates
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 
 SKIPPED=$(cat $TEMP_DIR/skipped 2>/dev/null || :)
